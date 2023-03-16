46365e02f717594f2295807dddff4799b1220bb5
Manish <my79220@gmail.com>
2023-03-16 22:04:01 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import PageNavbar from './components/PageNavbar'
+import PageTab from './components/PageTab';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <PageNavbar/>
+    <PageTab/>
+
+    </>
   );
 }
 
---
+++ src/components/PageTab.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import LargeTab from './Tabs/LargeTab';
+
+const Tabs = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+
+  return (
+    <>
+    <div className=" container mx-auto mt-20 flex flex-col md:flex-row">
+      <div className="md:w-1/4">
+        <ul className="flex border-b">
+          <li
+            className={`-mb-px mr-1 ${activeTab === 0 ? 'border-l border-t border-r rounded-t text-blue-700' : 'border-b border-gray-400 hover:border-gray-400'}`}
+            onClick={() => handleTabClick(0)}
+          >
+            <a
+              className={`bg-white rounded-t-lg inline-block py-2 px-4 font-semibold ${activeTab === 0 ? 'text-blue-700' : 'text-gray-500 hover:text-gray-800'}`}
+              href="#"
+            >
+              Tab 1
+            </a>
+          </li>
+          <li
+            className={`mr-1 ${activeTab === 1 ? 'border-l border-t border-r rounded-t text-blue-700' : 'border-b border-gray-400 hover:border-gray-400'}`}
+            onClick={() => handleTabClick(1)}
+          >
+            <a
+              className={`bg-white rounded-t-lg inline-block py-2 px-4 font-semibold ${activeTab === 1 ? 'text-blue-700' : 'text-gray-500 hover:text-gray-800'}`}
+              href="#"
+            >
+              Tab 2
+            </a>
+          </li>
+          <li
+            className={`mr-1 ${activeTab === 2 ? 'border-l border-t border-r rounded-t text-blue-700' : 'border-b border-gray-400 hover:border-gray-400'}`}
+            onClick={() => handleTabClick(2)}
+          >
+            <a
+              className={`bg-white rounded-t-lg inline-block py-2 px-4 font-semibold ${activeTab === 2 ? 'text-blue-700' : 'text-gray-500 hover:text-gray-800'}`}
+              href="#"
+            >
+              Tab 3
+            </a>
+          </li>
+        </ul>
+      </div>
+      </div>
+      <div className="flex mt-10 flex-col md:flex-row">
+
+      <div className="container mx-auto md:w-3/4">
+        <div className="p-4">
+          {activeTab === 0 && (
+            <div>
+              <LargeTab/>
+            </div>
+          )}
+          {activeTab === 1 && (
+            <div>
+              <p>Content of Tab 2</p>
+              <p>Add new components here</p>
+            </div>
+          )}
+          {activeTab === 2 && (
+            <div>
+              <p>Content of Tab 3</p>
+              <p>Add new components here</p>
+            </div>
+          )}
+        </div>
+      </div>
+      </div>
+      </>
+  );
+};
+
+export default Tabs;
